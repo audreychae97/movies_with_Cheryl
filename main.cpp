c62966add4e505a9c22b8f7b2ca7239afa2c30b7
audrey chae <audreychae97@hotmail.com>
2018-03-10 14:05:19 -0800

    updated features to store.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include "store.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main() {
 
-    Store* store = new Store;
-
+    Store * store = new Store("data4customers.txt");
+//
     store->loadMovieList("data4movies.txt");
     store->loadCustList("data4customers.txt");
-    store->readCommands("data4commands.txt");
-
+    //store->readCommands("data4commands.txt");
+    store->getHashSize();
 
     return 0;
 }
